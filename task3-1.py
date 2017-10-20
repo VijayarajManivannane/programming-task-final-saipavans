a47811e16badfe8bab8afb98270f093ba718a488
saipavans <sp403250@dal.ca>
2017-10-20 14:03:01 -0300

    completed task 3-1, modified config file is added, diff taken and added for reference

---
+++ task3-1.py
@@ -0,0 +1,16 @@
+config_file_path = "./running-config.cfg"
+output_file = "./config-modified.cfg"
+find_pattern = "172."
+replace_pattern = "192."
+
+def main():
+    fin = open(config_file_path)
+    fout = open(output_file,'w')
+    for line in fin:
+        line_modified = line.replace(find_pattern,replace_pattern)
+        fout.write(line_modified)
+    fin.close()
+    fout.close()
+
+if __name__ == '__main__':
+    main()
